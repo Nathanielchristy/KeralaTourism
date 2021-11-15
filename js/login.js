11ffc93a1cc9d6d652bb1820491542d83488392c
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2021-11-15 11:50:33 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,21 +1,18 @@
-
-function validate(){
-    let email=document.getElementById("email").value;
-    let password=document.getElementById("password").value;
-    let regexpemail = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+)\.([a-z]{2,3})(.[a-z]{2,3})?$/
-    if(email===""||password===""){
-        alert("Flieds cannot be Empty");
-        return false;
-    }
-    if(regexpemail.test(email)){
-        emailvalid.style.backgroundColor = 'green';
-        emailvalid.textContent = 'Email is valid';
-
-    }
-    else{
-        emailvalid.style.backgroundColor = 'red';
-        emailvalid.textContent = 'Email is Not valid';
-        return false;
-    }
-
+function validate() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let regexpemail =
+    /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+)\.([a-z]{2,3})(.[a-z]{2,3})?$/;
+  if (email === "" || password === "") {
+    alert("Flieds cannot be Empty");
+    return false;
+  }
+  if (regexpemail.test(email)) {
+    emailvalid.style.backgroundColor = "green";
+    emailvalid.textContent = "Email is valid";
+  } else {
+    emailvalid.style.backgroundColor = "red";
+    emailvalid.textContent = "Email is Not valid";
+    return false;
+  }
 }
